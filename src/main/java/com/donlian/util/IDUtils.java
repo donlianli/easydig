697fd9e4eeab41064b8d03639785b80ce8b17fe7
lidongliang <lidongliang@55tuan.com>
2013-09-29 15:44:54 +0800

    提交代码

--- src/main/java/com/donlian/util/IDUtils.java
+++ src/main/java/com/donlian/util/IDUtils.java
@@ -7,7 +7,7 @@ public class IDUtils {
 	 * @throws Exception 
 	 */
 	public static void main(String[] args) throws Exception {
-		System.out.println(new JeeheDES().decrypt("f67cd5db01bddbaf"));;
+		System.out.println(new JeeheDES().decrypt("4f40665e3f2b4dff"));;
 		System.out.println(new JeeheDES().encrypt("673059"));;
 	}
 }

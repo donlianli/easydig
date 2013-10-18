dbf266094a3ec21a1d5e5aa653f03b9330e26950
lidongliang <lidongliang@55tuan.com>
2013-10-18 18:01:34 +0800

    增加全量备份

--- src/main/java/com/donlianli/es/admin/BackupTest.java
+++ src/main/java/com/donlianli/es/admin/BackupTest.java
@@ -1,5 +1,7 @@
 package com.donlianli.es.admin;
 
+import java.util.List;
+
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.Client;
@@ -21,17 +23,31 @@ import com.donlianli.es.ESUtils;
 public class BackupTest {
 	public static final String backupDir = "d:/temp/es";
 	public static void main(String[] args) {
-		backupMapping("goods_city_1");
-		backupDoc("goods_city_1");
+//		backupMapping("goods_city_1");
+//		backupDoc("goods_city_1");
+		backupAll();
 	}
 	/**
 	 * 备份结构定义
 	 * @param string
 	 */
-	public static void backupMapping(String string) {
+	public static void backupAll() {
+		//备份所有的mapping,并返回index列表
+		List<String> indexes = GetMapping2Test.getAllIndexes();
+		for(String indexName : indexes){
+			backupDoc(indexName);
+			System.out.println(indexName + " backup over");
+		}
+	}
+	
+	/**
+	 * 备份结构定义
+	 * @param string
+	 */
+	public static void backupMapping(String indexName) {
 		//获得所有的index下面的mapping
 		//备份mapping的名称和结构定义
-		
+		String mapping = GetMapping2Test.getMapping(indexName);
 	}
 	public static void backupDoc(String indexName){
 		long startTime = System.currentTimeMillis();
@@ -53,22 +69,28 @@ public class BackupTest {
 			System.out.println(hit.getSourceAsString());
 		}
 		System.out.println("------------------------------");
-		while (fetchCount < docCount) {
+		int failedCount=0;
+		while (fetchCount < docCount && failedCount<3) {
 			// 只要取不够，一直取
 			// 使用上次的scrollId继续访问
 			searchResponse = esClient
 					.prepareSearchScroll(searchResponse.getScrollId())
 					.setScroll(TimeValue.timeValueMinutes(8)).execute()
 					.actionGet();
-
-			// System.out.println(searchResponse.getHits().getTotalHits());
-			fetchCount += searchResponse.getHits().hits().length;
-			if (fetchCount % 1000 == 0) {
-				System.out.println(fetchCount + " fetched");
+			if(searchResponse.getHits().hits().length==0){
+				failedCount++;
 			}
-			for (SearchHit hit : searchResponse.getHits()) {
-				// 打印数据或输出到文件
-				// System.out.println(hit.getSourceAsString());
+			else {
+				// System.out.println(searchResponse.getHits().getTotalHits());
+				fetchCount += searchResponse.getHits().hits().length;
+				if (fetchCount % 1000 == 0) {
+					System.out.println(fetchCount + " fetched");
+				}
+				for (SearchHit hit : searchResponse.getHits()) {
+					// 打印数据或输出到文件
+//					 System.out.println(hit.getType());
+//					 System.out.println(hit.getSourceAsString());
+				}
 			}
 		}
 		System.out.println("fetch count:" + fetchCount);
--- src/main/java/com/donlianli/es/admin/GetMapping2Test.java
+++ src/main/java/com/donlianli/es/admin/GetMapping2Test.java
@@ -2,12 +2,15 @@ package com.donlianli.es.admin;
 
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.elasticsearch.client.Client;
 import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.metadata.IndexMetaData;
 import org.elasticsearch.cluster.metadata.MappingMetaData;
+import org.elasticsearch.cluster.metadata.MetaData;
 
 import com.donlianli.es.ESUtils;
 /**
@@ -17,10 +20,15 @@ import com.donlianli.es.ESUtils;
 public class GetMapping2Test {
 
 	public static void main(String[] args) {
+//		String json = getMapping("goods_city_1");
+		getClusterMapping();
+	}
+
+	public static String getMapping(String string) {
 		String indexName = "goods_city_1";
 		Client client = ESUtils.getClient();
 		ClusterState cs = client.admin().cluster().prepareState()
-				.setFilterIndices()
+				.setFilterIndices(indexName)
 				.execute().actionGet().getState();
 		IndexMetaData imd = cs.getMetaData().index(indexName);	
 		Map<String, MappingMetaData> typeMap = imd.mappings();
@@ -35,5 +43,48 @@ public class GetMapping2Test {
 				e.printStackTrace();
 			}	
 		}
+		return null;
+	}
+	
+	public static String getClusterMapping() {
+		Client client = ESUtils.getClient();
+		ClusterState cs = client.admin().cluster().prepareState()
+				.execute().actionGet().getState();
+		MetaData meta = cs.getMetaData();	
+		Map<String, IndexMetaData> indexMap = meta.indices();
+		for(Map.Entry<String, IndexMetaData> iEntry : indexMap.entrySet()){
+			//索引名称
+			String indexName = iEntry.getKey();
+			System.out.println("indexName:"+indexName);
+			IndexMetaData imd = iEntry.getValue();
+			Map<String, MappingMetaData> typeMap = imd.mappings();
+			for(Map.Entry<String, MappingMetaData> entry : typeMap.entrySet()){
+				//type名称
+				String typeName = entry.getKey();
+				MappingMetaData typeDesc = entry.getValue();
+				try {
+					//这个本身就是一个json格式
+					System.out.println(typeName+":"+typeDesc.getSourceAsMap().toString());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}	
+			}
+		}
+		return null;
+	}
+	
+	public static List<String> getAllIndexes() {
+		Client client = ESUtils.getClient();
+		ClusterState cs = client.admin().cluster().prepareState()
+				.execute().actionGet().getState();
+		MetaData meta = cs.getMetaData();	
+		Map<String, IndexMetaData> indexMap = meta.indices();
+		List<String> indexList = new ArrayList<String>();
+		for(Map.Entry<String, IndexMetaData> iEntry : indexMap.entrySet()){
+			//索引名称
+			String indexName = iEntry.getKey();
+			indexList.add(indexName);
+		}
+		return indexList;
 	}
 }

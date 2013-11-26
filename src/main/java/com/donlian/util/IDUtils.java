c7006c81fef3a750e394cf27eefefdaa69fa9b17
lidongliang <lidongliang@55tuan.com>
2013-11-26 09:33:09 +0800

    提交测试锁和序列化的测试代码

--- src/main/java/com/donlian/util/IDUtils.java
+++ src/main/java/com/donlian/util/IDUtils.java
@@ -7,7 +7,7 @@ public class IDUtils {
 	 * @throws Exception 
 	 */
 	public static void main(String[] args) throws Exception {
-		System.out.println(new JeeheDES().decrypt("44bc2381d8e6813b"));;
+		System.out.println(new JeeheDES().decrypt("8e5be106ae9a4775"));;
 		System.out.println(new JeeheDES().encrypt("673059"));;
 	}
 }

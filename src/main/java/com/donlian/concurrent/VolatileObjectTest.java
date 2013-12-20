be96c059fb21b32099c5152f20b9802ca1838920
lidongliang <lidongliang@55tuan.com>
2013-12-20 17:02:01 +0800

    增加注释

--- src/main/java/com/donlian/concurrent/VolatileObjectTest.java
+++ src/main/java/com/donlian/concurrent/VolatileObjectTest.java
@@ -2,11 +2,16 @@
 package com.donlian.concurrent;
 
 /**
- * @author ThrilleR 
  * 必须要加上 -server才能生效，因为在-server下程序运行速度很快做过优化，才会出现stop不了的情况。
  * 在Object上加上volatile就可见了。
  * 
- * 这个例子说明：暂时无解
+ * 这个例子不能说明volatile关键字修饰对象，就会读取到对象域的最新值。之所以在server模式下面能够停止，也
+ * 只是跟具体的虚拟机实现有关系。
+ * volatile修饰关键字只能保证对象引用a的最新值及对象a的发布完整性。
+ * 
+ * 之所以线程没有终止，估计是因为新启动的线程没有跟主存同步。
+ * 
+ * 这个程序在单核的cpu上面也没有终止，说明编译器在编译代码的时候就已经做了优化
  */
 public class VolatileObjectTest implements Runnable {
 	private  ObjectA a; // 加上volatile 就可以正常结束While循环了
@@ -61,6 +66,5 @@ class ObjectA {
 	public void setFlag(boolean flag) {
 		this.flag = flag;
 	}
-
 }
 

c5335418908198494dab9f02b03314297c052b5c
lidongliang <donlianli@126.com>
2014-04-23 15:43:37 +0800

    提交

--- src/main/java/com/donlian/jdk/BinSafeSting.java
+++ src/main/java/com/donlian/jdk/BinSafeSting.java
@@ -1,7 +1,13 @@
 package com.donlian.jdk;
-
+/**
+ * 这里\0，并不是ascii码的控制字符0，而是转换
+ * 成了可见字符ascii字符32。跟c语言的\0还不是一样的。
+ * @author donlianli
+ *
+ */
 public class BinSafeSting {
 	public static void main(String[] args) {
+		
 		String s="Hello\0World";
 		s.length();
 		System.out.println(s);
---
+++ src/main/java/com/donlian/jdk/Supliments.java
@@ -0,0 +1,10 @@
+package com.donlian.jdk;
+
+public class Supliments {
+
+	public static void main(String[] args) {
+		byte a = -128;
+		System.out.println(a);
+	}
+
+}
